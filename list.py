03ab6f5a91e7f91a94dae87c1d3ce0b4d64faff9
premanand98123 <premkr98123@gmail.com>
2024-01-11 19:55:23 +0530

    11-01-2024

--- list.py
+++ list.py
@@ -1,1 +1,31 @@
-print("hey there im using python")
+# print("hey there im using python")
+
+'''
+Q- print the sum of n numbers
+
+'''
+
+# n = int(input("Enter a number "))
+# # print(sum(list))
+
+# s =int(( n * (n+1))/2)
+
+# print (s)
+
+
+'''
+Q- min element for list with its index
+'''
+
+list = []
+
+n = int(input("Enter the size of the list: "))
+
+print("Enter the values for the list:")
+
+for i in range(0, n):
+    element = int(input())
+    list.append(element)
+
+print("The list is:", list)
+
